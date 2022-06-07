2610cb8ce9ea6718d69ad686c4e3c761ed625711
miguel <mglnglx@MacBook-Air.local>
2022-06-07 14:03:12 +0200

    branch

---
+++ script.js
@@ -0,0 +1,18 @@
+const loadText = document.querySelector(".loading-text");
+const container = document.querySelector(".container");
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+    loadText.classList.add("hidden");
+    container.classList.remove("hide-body");
+  }
+
+  loadText.innerText = `loading ${load}`;
+}
